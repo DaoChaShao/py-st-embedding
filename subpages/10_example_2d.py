fb4b9b8d07672961feb200c0bc231be3e06d7a96
DaoChaShao <27882272@qq.com>
2025-02-22 11:06:03 +1300

    fix: update imports and enhance scatter plot parameters

--- subpages/10_example_2d.py
+++ subpages/10_example_2d.py
@@ -1,11 +1,14 @@
 from pandas import DataFrame
 from streamlit import plotly_chart, data_editor
-from utilities.datasets import feature_data_2d
-from utilities.tools import scatter_2d
 
+from utilis.datasets import feature_data_2d
+from utilis.params import params_example_2d
+from utilis.tools import scatter_2d
+
+point_size, font_size = params_example_2d()
 features_2d: DataFrame = feature_data_2d()
 
 data_editor(features_2d, disabled=True, hide_index=True, use_container_width=True)
 
-chart_2d = scatter_2d(features_2d)
+chart_2d = scatter_2d(features_2d, point_size, font_size)
 plotly_chart(chart_2d, use_container_width=True)
