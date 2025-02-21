c68f7ffcca478bfed895da8feaf4689f41f3fef9
DaoChaShao <27882272@qq.com>
2025-02-22 12:59:40 +1300

    feat: add sidebar for embedding model configuration with hyperparameters

--- utilis/params.py
+++ utilis/params.py
@@ -1,4 +1,5 @@
-from streamlit import sidebar, header, slider, caption
+from streamlit import (sidebar, header, slider, caption, selectbox,
+                       segmented_control)
 
 
 def params_example_2d() -> tuple[int, int]:
@@ -93,3 +94,25 @@ def params_umap() -> tuple[int, int, int]:
         )
         caption(f"The size of the text in the scatter plot is {font_size}")
         return feature_dims, point_size, font_size
+
+
+def parmas_embedding():
+    """ Set up the sidebar for the embedding model """
+    with sidebar:
+        header("HyperParameters")
+        options_box: list = ["iic/nlp_gte_sentence-embedding_chinese-large"]
+        model: str = selectbox(
+            "Model ID", options_box, disabled=True,
+            help="Select the model for embedding",
+        )
+        caption(f"The dims of the model you selected is **1024**")
+        match model:
+            case "iic/nlp_gte_sentence-embedding_chinese-large":
+                dimensions: int = 1024
+        options_seg: list = [128, 256, 512]
+        length: int = segmented_control(
+            "Sequence Length", options_seg, default=256, selection_mode="single",
+            help="Select the sequence length for the model",
+        )
+        caption(f"The sequence length for the model is {length}")
+        return model, dimensions, length
