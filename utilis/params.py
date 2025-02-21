c9c5a3a1c96f70648946e5e6f610dc25c3377680
DaoChaShao <27882272@qq.com>
2025-02-22 11:12:16 +1300

    feat: add sidebar parameters for 3D scatter visualization

--- utilis/params.py
+++ utilis/params.py
@@ -23,6 +23,28 @@ def params_example_2d() -> tuple[int, int]:
         return point_size, font_size
 
 
+def params_example_3d() -> tuple[int, int]:
+    """ Set up the sidebar for the 3D scatter visualization
+
+    :return: point_size, font_size
+    """
+    with sidebar:
+        header("Parameters of 3D Scatter")
+        point_size: int = slider(
+            "Point Size",
+            min_value=5, max_value=20, value=10, step=1, format="%d",
+            help="The size of the points in the scatter plot",
+        )
+        caption(f"The size of the points in the scatter plot is {point_size}")
+        font_size: int = slider(
+            "Font Size",
+            min_value=10, max_value=20, value=12, step=1, format="%d",
+            help="The size of the text in the scatter plot",
+        )
+        caption(f"The size of the text in the scatter plot is {font_size}")
+        return point_size, font_size
+
+
 def params_umap():
     """ Set up the sidebar for the UMAP visualization """
     params: dict = {}
