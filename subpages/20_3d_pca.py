7e66b6b17d29a0952356fe42e01766e917ea2d23
DaoChaShao <27882272@qq.com>
2025-02-22 11:24:44 +1300

    feat: integrate customizable parameters for PCA scatter plot

--- subpages/20_3d_pca.py
+++ subpages/20_3d_pca.py
@@ -1,9 +1,12 @@
 from pandas import DataFrame
 from streamlit import plotly_chart, data_editor
+
 from utilis.datasets import feature_data
+from utilis.params import params_pca
 from utilis.tools import PCALearnerDimensionsReducer, scatter_3d_nor
 
-features: DataFrame = feature_data(8)
+feature_dims, point_size, font_size = params_pca()
+features: DataFrame = feature_data(feature_dims)
 
 data_editor(features, disabled=True, hide_index=True, use_container_width=True)
 
@@ -11,5 +14,5 @@ data_editor(features, disabled=True, hide_index=True, use_container_width=True)
 features_3d_pca = PCALearnerDimensionsReducer(dimensions=3).decrease(features)
 
 # Plot the result in 3D
-chart_3d_pca = scatter_3d_nor(features_3d_pca)
+chart_3d_pca = scatter_3d_nor(features_3d_pca, point_size, font_size)
 plotly_chart(chart_3d_pca, use_container_width=True)
