65f1e8735dec41e7026ac2934423b4c7298b09fc
DaoChaShao <27882272@qq.com>
2025-02-22 12:59:46 +1300

    feat: implement sentence embedding model selection with modelscope

--- utilis/models.py
+++ utilis/models.py
@@ -1,3 +1,13 @@
-def model_4_embedding():
+from modelscope.models import Model
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+
+
+def model_cn_embedding_sentences(model_id: str, sequence_length: int):
     """ Select the model for embedding """
-    pass
+    pipeline_sentences = pipeline(
+        Tasks.sentence_embedding,
+        model=model_id,
+        sequence_length=sequence_length,
+    )
+    return pipeline_sentences
