274b6dc4bc4b52ae50820351f5252df094eb356a
DaoChaShao <27882272@qq.com>
2025-02-22 11:29:45 +1300

    feat: update 3D UMAP to accept feature dimensions, point size, and font size parameters

--- subpages/21_3d_umap.py
+++ subpages/21_3d_umap.py
@@ -1,12 +1,13 @@
 from pandas import DataFrame
 from streamlit import plotly_chart, data_editor
+
 from utilis.datasets import feature_data
 from utilis.params import params_umap
 from utilis.tools import UMAPNonlinearDimensionsReducer, scatter_3d_nor
 
 # Generate some sample data
-amount: int = params_umap()["amount"]
-features: DataFrame = feature_data(amount)
+feature_dims, point_size, font_size = params_umap()
+features: DataFrame = feature_data(feature_dims)
 
 data_editor(features, disabled=True, hide_index=True, use_container_width=True)
 
@@ -14,5 +15,5 @@ data_editor(features, disabled=True, hide_index=True, use_container_width=True)
 features_3d_umap = UMAPNonlinearDimensionsReducer(dimensions=3, neighbours=5).decrease(features)
 
 # Plot the result in 3D
-chart_3d_umap = scatter_3d_nor(features_3d_umap)
+chart_3d_umap = scatter_3d_nor(features_3d_umap, point_size, font_size)
 plotly_chart(chart_3d_umap, use_container_width=True)
