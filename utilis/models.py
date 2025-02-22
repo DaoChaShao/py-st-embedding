bb77339452e9990279037d8668f5931d78f1a3c7
DaoChaShao <27882272@qq.com>
2025-02-22 16:23:51 +1300

    docs: update docstring for model_cn_embedding_sentences to clarify purpose and usage

--- utilis/models.py
+++ utilis/models.py
@@ -4,7 +4,13 @@ from modelscope.utils.constant import Tasks
 
 
 def model_cn_embedding_sentences(model_id: str, sequence_length: int):
-    """ Select the model for embedding """
+    """
+    Select the embedding model for Chinese sentences
+        https://www.modelscope.cn/models/iic/nlp_gte_sentence-embedding_chinese-large/summary
+    :param model_id: The model ID for the embedding
+    :param sequence_length: The sequence length for the model
+    :return: The pipeline for embedding sentences
+    """
     pipeline_sentences = pipeline(
         Tasks.sentence_embedding,
         model=model_id,
